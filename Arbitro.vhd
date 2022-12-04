0794c922d413d67e2e2160491f9c54fe827c3461
PauloVLB <paulo.borges@imd.ufrn.br>
2022-12-04 17:30:14 -0300

    Contador e buffer

--- Arbitro.vhd
+++ Arbitro.vhd
@@ -8,24 +8,28 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity Arbitro is
-    Port (  clk : in std_logic;
+    Port (  clk : in bit;
             --rst : in std_logic;
             reqn : in bit;
             reqs : in bit;
             reql : in bit;
             reqo : in bit;
 
-            act : out bit_vector(0 downto 3);
+            actn : out bit;
+            acts : out bit;
+            actl : out bit;
+            acto : out bit;
             
-            sel : out std_logic_vector(0 to 2) --SELECTOR
+            sel : out bit_vector(0 to 2) --SELECTOR
     );
 end entity Arbitro;
 
 
 architecture Behavioral of Arbitro is
     signal ptr : std_logic_vector(0 to 1) := "00";
-    signal rest : bit := '1';
+    signal reset : bit := '1';
     signal reqn2, reqs2, reql2, reqo2 : bit;
+    signal act : bit_vector(0 to 3) := "0000";
 
 begin
     mem   : entity work.Registrador4Bits
@@ -35,8 +39,8 @@ begin
         d(2) => reql,
         d(3) => reqo,
         clk => clk, -- clock
-        clrn: => '1',
-        ena : '1',
+        clrn => '1',
+        ena => '1',
         q(0) => reqn2,
         q(1) => reqs2,
         q(2) => reql2,
@@ -44,60 +48,51 @@ begin
         );-- data output
 
     count :   entity work.Contador
-    port map (  d => '0', 
+    port map (  d => "00", 
                 clk => clk,
-                clrn => reset,
-                ena: => andar,
-                load: => '0',
-                q => ptr )
+                clrn => '1',
+                ena => '1',
+                load => '0',
+                q => ptr );
     process(clk)
-    if ptr = "11" then reset <= '0';
-
-    if ptr = "00" then
-        if reqn2 = '1' then
-            act <= "1000";
-            sel <= "001"; -- "001" corresponde a entrada norte no demux
-        else
-            act <= "0000";
-            sel <= "000";
-        end if;
-    elsif ptr = "01" then
-        if reqs2 = '1' then
-            act <= "0100";
-            sel <= "010"; -- "010" corresponde a entrada sul no demux
-        else
-            act <= "0000";
-            sel <= "000";
-        end if;
-    elsif ptr = "10" then
-        if reql2 = '1' then
-            act <= "0010";
-            sel <= "011"; -- "011" corresponde a entrada leste no demux
-        else
-            act <= "0000";
-            sel <= "000";
-        end if;
-    elsif ptr = "11" then
-        if reqo2 = '1' then
-            act <= "0001";
-            sel <= "100"; -- "100" corresponde a entrada oeste no demux
-        else
-            act <= "0000";
-            sel <= "000";
-        end if;
-    end if;
-    
-    end 
-    process;
-            
-
-    
-
     begin
-        if rising_edge(clk) then
+        if (clk'EVENT AND clk='1') then
+            if ptr = "00" then
+                if reqn = '1' then
+                    act <= "1000";
+                else
+                    act <= "0000";
+                end if;
+            elsif ptr = "01" then
+                if reqs = '1' then
+                    act <= "0100";
+                else
+                    act <= "0000";
+                end if;
+            elsif ptr = "10" then
+                if reql = '1' then
+                    act <= "0010";
+                else
+                    act <= "0000";
+                end if;
+            elsif ptr = "11" then
+                if reqo = '1' then
+                    act <= "0001";
+                else
+                    act <= "0000";
+                end if;
+            end if;
         end if;
-    end 
-    process;
-    act <= req;
-    sel <= ptr;
+    end process;
+
+    actn <= act(0);
+    acts <= act(1);
+    actl <= act(2);
+    acto <= act(3);
+    
+    sel <= "001" when act(0) = '1' else
+           "010" when act(1) = '1' else
+           "011" when act(2) = '1' else
+           "100" when act(3) = '1' else
+           "000";
 end architecture Behavioral;
